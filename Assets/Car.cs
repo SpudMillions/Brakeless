d5e1f7c3b24bff943be7cc15d0e2a182d771e987
Gaston Rider <joshuaadams18@gmail.com>
2017-12-10 11:40:21 -0500

    added serialized fields so designer can control turning speed and acceleration from unity gui

--- Assets/Car.cs
+++ Assets/Car.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 
 public class Car : MonoBehaviour {
 
-	Rigidbody rigidBody;
+    [Tooltip("Controls speed at which car can turn left and right")]
+    [SerializeField] float rcsBoost = 100f;
+
+    [Tooltip("Controls speed at which car accelerates")]
+    [SerializeField]
+    float mainBoost = 40f;
+
+    Rigidbody rigidBody;
 	AudioSource audioSource;
 
 	// Use this for initialization
@@ -24,7 +31,7 @@ public class Car : MonoBehaviour {
 	{
 		if (Input.GetKey(KeyCode.Space))
 		{
-			rigidBody.AddRelativeForce(Vector3.up);
+			rigidBody.AddRelativeForce(Vector3.up * mainBoost);
 			if (!audioSource.isPlaying)
 			{
 				audioSource.Play();
@@ -38,17 +45,21 @@ public class Car : MonoBehaviour {
 	}
 	private void Turn()
 	{
-        rigidBody.freezeRotation = true; // take manual control of rotation
-		if (Input.GetKey(KeyCode.A))
+		rigidBody.freezeRotation = true; // take manual control of rotation
+
+        // we want to be able to capture how fast to rotate our turn based on our rcsBoost
+        float rotationThisFrame = rcsBoost * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
 		{
-			transform.Rotate(Vector3.forward);
+			transform.Rotate(Vector3.forward * rotationThisFrame);
 		}
 		else if (Input.GetKey(KeyCode.D))
 		{
-			transform.Rotate(Vector3.back);
+            transform.Rotate(Vector3.back * rotationThisFrame);
 		}
 
-        rigidBody.freezeRotation = false; // release manual control
+		rigidBody.freezeRotation = false; // release manual control
 	}
 
 	
