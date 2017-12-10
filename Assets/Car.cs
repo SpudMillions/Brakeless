51b0e90f46778ee9fb278b7b6da3af36309b4557
Gaston Rider <joshuaadams18@gmail.com>
2017-12-10 12:00:15 -0500

    refactor naming, added tags for friendly, fuel, and default player death

--- Assets/Car.cs
+++ Assets/Car.cs
@@ -5,33 +5,33 @@ using UnityEngine;
 
 public class Car : MonoBehaviour {
 
-    [Tooltip("Controls speed at which car can turn left and right")]
-    [SerializeField] float rcsBoost = 100f;
+	[Tooltip("Controls speed at which car can turn left and right")]
+	[SerializeField] private float turnSpeed = 100f;
 
-    [Tooltip("Controls speed at which car accelerates")]
-    [SerializeField]
-    float mainBoost = 40f;
+	[Tooltip("Controls speed at which car accelerates")]
+	[SerializeField]
+	private float accelerationSpeed = 40f;
 
-    Rigidbody rigidBody;
+	Rigidbody rigidBody;
 	AudioSource audioSource;
 
 	// Use this for initialization
-	void Start () {
+	private void Start () {
 		rigidBody = GetComponent<Rigidbody>();
 		audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		Boost();
+	private void Update () {
+		Accelerate();
 		Turn();
 	}
 
-	private void Boost()
+	private void Accelerate()
 	{
 		if (Input.GetKey(KeyCode.Space))
 		{
-			rigidBody.AddRelativeForce(Vector3.up * mainBoost);
+			rigidBody.AddRelativeForce(Vector3.up * accelerationSpeed);
 			if (!audioSource.isPlaying)
 			{
 				audioSource.Play();
@@ -47,20 +47,36 @@ public class Car : MonoBehaviour {
 	{
 		rigidBody.freezeRotation = true; // take manual control of rotation
 
-        // we want to be able to capture how fast to rotate our turn based on our rcsBoost
-        float rotationThisFrame = rcsBoost * Time.deltaTime;
+		// we want to be able to capture how fast to rotate our turn based on our rcsBoost
+		float rotationThisFrame = turnSpeed * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.A))
+		if (Input.GetKey(KeyCode.A))
 		{
 			transform.Rotate(Vector3.forward * rotationThisFrame);
 		}
 		else if (Input.GetKey(KeyCode.D))
 		{
-            transform.Rotate(Vector3.back * rotationThisFrame);
+			transform.Rotate(Vector3.back * rotationThisFrame);
 		}
 
 		rigidBody.freezeRotation = false; // release manual control
 	}
 
-	
+	private void OnCollisionEnter(Collision collision)
+	{
+		switch (collision.gameObject.tag)
+		{
+			case "Friendly":
+				//do nothing
+				break;
+			case "Fuel":
+				//add fuel
+				break;
+			default:
+				//dead
+				//todo: kill player
+				break;
+		}
+	}
+
 }
